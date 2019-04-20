221c1a272e3daef47513da8b3c1beb5c6539eaf4
Victor Nieves Sanchez <vnievess@gmail.com>
2019-04-20 18:15:16 +0200

    added some comments

--- src/broker/broker.c
+++ src/broker/broker.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "comun.h"
 
+/* incluye toda la funcionalidad del sistema, implementando las operaciones
+ * descritas en el apartado previo y gestionando las colas de mensajes.
+ * Recibe como argumento el número del puerto por el que prestará servicio.
+*/
 int main(int argc, char *argv[]){
     if(argc!=2) {
         fprintf(stderr, "Uso: %s puerto\n", argv[0]);
--- src/libpullMQ/libpullMQ.c
+++ src/libpullMQ/libpullMQ.c
@@ -1,16 +1,42 @@
 #include "comun.h"
 #include "pullMQ.h"
 
+// BROKER_HOST: nombre de la máquina donde ejecuta el broker.
+// BROKER_PORT: número de puerto TCP por el que está escuchando.
+
+/* 0 si la operación se realizó satisfactoriamente y
+ * un valor negativo en caso contrario
+ * cola: nombre de la cola
+ */
 int createMQ(const char *cola) {
 	return 0;
 }
+
+/* 0 si la operación se realizó satisfactoriamente y
+ * un valor negativo en caso contrario
+ * cola: nombre de la cola
+ */
 int destroyMQ(const char *cola){
 	return 0;
 }
+
+/* 0 si la operación se realizó satisfactoriamente y
+ * un valor negativo en caso contrario
+ * cola: nombre de la cola
+ * mensaje: mensaje
+ * tam: tamaño del mensaje
+ */
 int put(const char *cola, const void *mensaje, size_t tam){
 	return 0;
 }
+
+/* 0 si la operación se realizó satisfactoriamente y
+ * un valor negativo en caso contrario
+ * cola: nombre de la cola
+ * mensaje: mensaje
+ * tam: tamaño del mensaje
+ * blocking: indica si la cola es boqueante
+ */
 int get(const char *cola, void **mensaje, size_t *tam, bool blocking){
 	return 0;
 }
-
