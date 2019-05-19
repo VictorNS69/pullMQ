14491d9fb975155f0b2c0c10573d47a5fc912ee4
Victor Nieves Sanchez <vnievess@gmail.com>
2019-05-19 21:59:32 +0200

    tried to implement libpullMQ

--- src/broker/comun.h
+++ src/broker/comun.h
@@ -3,6 +3,40 @@
  * necesitar compartir el broker y la biblioteca, si es que las hubiera.
  */
 
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>		
+#include <string.h>		
+#include <sys/socket.h> 
+#include <arpa/inet.h>  
+#include <netdb.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdbool.h>
+
+#include <netdb.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <string.h>
+#include <unistd.h>
+
+#define CREATE 1
+#define DESTROY 2
+#define PUT 3
+#define GET 4
+
+typedef struct{
+	int operation;
+	size_t queue_name_len;
+	char *queue_name;
+	size_t msg_len;
+	void *msg;
+	bool blocking;
+} Request;
+
 struct Node{
     char *msg;
     struct Node *next;
